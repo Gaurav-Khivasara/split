6f41dc5e5cd7434c3df5213e7ca7842a53c21a8a
Gaurav-Khivasara <gkgauravgk123@gmail.com>
2025-08-14 05:09:49 +0530

    Commented unwanted bcrypt import and Added get user by token API

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const db = require("../config/db");
 
 router.get("", (req, res) => {
   res.status(200).json({ message: "This is users route" });
+});
+
+router.get("/get-by-token", async (req, res) => {
+  try {
+    const user = (await db.query(
+      `SELECT id, name, email, avatar FROM users WHERE id = $1`,
+      [req.user.id]
+    )).rows[0];
+
+    if (!user) {
+      return res.json(404).json({ message: "User not found!" });
+    }
+
+    res.json({ user });
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    
+    res.status(500).json({ message: "An error occured!" });
+  }
 });
 
 // router.get("/register", (req, res) => {
