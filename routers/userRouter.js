a27b9857c73c072137b9134b194ce32c41603c6c
Gaurav-Khivasara <gkgauravgk123@gmail.com>
2025-08-28 16:03:55 +0530

    Removed unwanted comments of /register URL as using Google OAuth, Debugged res.json().json()

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -16,7 +16,7 @@ router.get("/get-by-token", async (req, res) => {
     )).rows[0];
 
     if (!user) {
-      return res.json(404).json({ message: "User not found!" });
+      return res.status(404).json({ message: "User not found!" });
     }
 
     res.json({ user });
@@ -26,91 +26,5 @@ router.get("/get-by-token", async (req, res) => {
     res.status(500).json({ message: "An error occured!" });
   }
 });
-
-// router.get("/register", (req, res) => {
-//   res.send(`
-//     <html>
-//       <head>
-//         <title>User Registration</title>
-//       </head>
-//       <body>
-//         <h2>User Registration</h2>
-
-//         <form action="/users/register" method="POST" >
-//           <label for="name">Name:</label>
-//           <input type="text" id="name" name="name" required />
-//           <br /><br />
-
-//           <label for="email">Email:</label>
-//           <input type="email" id="email" name="email" required />
-//           <br /><br />
-
-//           <label for="password">Password:</label>
-//           <input type="password" id="password" name="password" required />
-//           <br /><br />
-
-//           <input type="submit" value="Submit" />
-//         </form>
-
-//         <script>
-//           function validate(event) {
-//             const pass = document.getElementById("password").value;
-
-//             const hasUpper = /[A-Z]/.test(pass);
-//             const hasLower = /[a-z]/.test(pass);
-//             const hasNumber = /[0-9]/.test(pass);
-//             const hasSymbol = /[^A-Za-z0-9]/.test(pass);
-
-//             if (pass.length < 8 ||
-//               !hasUpper ||
-//               !hasLower ||
-//               !hasNumber ||
-//               !hasSymbol
-//             ) {
-//               alert("Choose a strong password! (Capital, Small, Number, Symbol, Min 8)");
-//               event.preventDefault();
-//               return false;
-//             }
-
-//             return true;
-//           }
-//         </script>
-//       </body>
-//     </html>
-//   `);
-// });
-
-// Not required as using Google OAuth 2.0
-// router.post("/register", async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
-//     console.log("Request -- User details:", name + ", " + email + ", " + password);
-
-//     // TODO 0
-//     // passwordHash and password_hash not needed as using Google OAuth 2.0
-//     const passwordHash = await bcrypt.hash(password, 10);
-
-//     // const result = await db.query(
-//     await db.query(
-//       `INSERT INTO users (name, email, password_hash)
-//       VALUES ($1, $2, $3) RETURNING *`,
-//       [name, email, passwordHash]
-//     );
-
-//     res.status(201).json({
-//       message: "User created successfully!",
-//       name,
-//       email
-//     });
-//   } catch (err) {
-//     console.error("Error inserting user:", err.message);
-
-//     if (err.code === "23505") {
-//       res.status(409).json({ message: "Email already exist!" });
-//     } else {
-//       res.status(500).json({ message: "An error occured!" });
-//     }
-//   }
-// });
 
 module.exports = router;
